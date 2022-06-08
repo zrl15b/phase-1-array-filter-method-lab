37e2ac470e5207f10796ad181c7062f508db17a1
Zachary <zacharylampi96@gmail.com>
2022-06-08 19:52:36 -0400

    commit tree dd20e6f4df030cf6b623f84bdaf30fbb0d28dd01

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+  
+      function findMatching(arr, query) {
+        return arr.filter(function(item) {
+          return item.toLowerCase().indexOf(query.toLowerCase()) !== -1
+        })
+      }
+
+      function fuzzyMatch(arr, query) {
+        return arr.filter(function(item) { 
+            if (item.toLowerCase().startsWith( query.toLowerCase())) { 
+          return item.toLowerCase().indexOf(query.toLowerCase()) !== -1
+         }
+          })
+      }
+
+     
+      function matchName(arr, query) {
+        return arr.filter(function(item) {
+          return item.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+        })
+      }
